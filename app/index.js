2be52eb9814e8388580fae63c04bb9e037729522
kieran jones <Kieran Jones>
2017-07-10 11:43:59 -0700

    Import intro Example - Kieran Jones

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import * as math from './math.js';
+import $ from 'jquery';
+
+const num1 = 6;
+const num2 = 5;
+const zero = 0;
+
+console.log(`The total of ${num1} and ${num2} is ${math.add(num1,num2)}`);
+console.log(`Divide by zero result: ${math.division(num1,zero)}`)
+
+console.log(`multiply ${num1} and ${num2}: ${math.multiply(num1,num2)}`)
+
+$('#root').append('<h1>Help Me</h1>')
---
+++ app/math.js
@@ -0,0 +1,16 @@
+export function add (x,y){
+	return x + y;
+}
+export function subtract (x,y){
+	return x - y;
+}
+export function multiply (x,y){
+	return x * y;
+}
+export function division (x,y){
+	if(y===0){
+		return "false"
+	} else {
+	return x / y;
+	}
+}
